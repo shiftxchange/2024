b1eeaef342e5a1d2bd18b6db0dbcfcca1c70f514
mrinalsharma14 <107413058+mrinalsharma14@users.noreply.github.com>
2024-03-24 14:54:34 +0400

    auto-gen header/footer

---
+++ assets/js/footer.js
@@ -0,0 +1,16 @@
+// Generate Footer
+var footer = document.createElement('footer');
+footer.id = 'footer';
+footer.innerHTML = `
+<div class="container">
+  <h4>ShiftXchange: A Practice-Based Approach to Rethinking Leadership in the Digital Era</h4>
+  <div class="social-links">
+    <a href="#" class="twitter"><i class="bx bi-twitter-x"></i></a>
+    <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+    <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+  </div>
+  <div class="credits">
+    Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+  </div>
+</div>`;
+document.body.appendChild(footer);
